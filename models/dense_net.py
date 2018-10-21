190de260e9b56b2dcb77149e9e1d1ca9354a23f7
idobronstein <idobron123@gmail.com>
2018-10-21 21:08:00 +0300

    add all

--- models/dense_net.py
+++ models/dense_net.py
@@ -137,7 +137,7 @@ class DenseNet:
 
     @property
     def model_identifier(self):
-        return "{}_growth_rate={}_depth={}_dataset_{}_weight_decay={}".format(
+        return "{}_growth_rate={}_depth={}_dataset_{}".format(
             self.model_type, self.growth_rate, self.depth, self.dataset_name, self.weight_decay)
 
     def save_model(self, global_step=None):
